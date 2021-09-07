b0d4b9fc8e84ed1f683a4f392b8b03a8bffc95c1
malikc <kkcn.mustafa@outlook.com>
2021-09-07 15:50:04 +0300

    randomrange change

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -8,7 +8,7 @@ public class Cube : MonoBehaviour
     
     void Start()
     {
-        transform.position = new Vector3(Random.Range(-6,10),Random.Range(-7,15),Random.Range(-13,13));
+        transform.position = new Vector3(Random.Range(-6,10),Random.Range(-7,14),Random.Range(-13,13));
         transform.localScale = Vector3.one * Random.Range(0.0f,5.0f);
         Material material = Renderer.material;
         material.color = new Color(Random.Range(0.0f,1.0001f), Random.Range(0.0f,1.0001f),Random.Range(0.0f,1.0001f), 0.4f);
